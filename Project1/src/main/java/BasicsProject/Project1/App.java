fdd777bd45cd2af6487829534e692307b8c931b5
rabih123 <rabih.hajj.12345@gmail.com>
2017-09-13 11:50:59 +0300

    gg

---
+++ Project1/src/main/java/BasicsProject/Project1/App.java
@@ -0,0 +1,16 @@
+package BasicsProject.Project1;
+
+import org.neo4j.driver.v1.Session;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
